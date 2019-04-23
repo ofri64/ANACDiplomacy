0b70746cd31c2c113870d61ef3d99e425e25300d
Ofri Kleinfeld <ofri64@gmail.com>
2019-04-23 22:42:00 +0300

    fixed bug with accumulating dmz deals

--- src/ddejonge/bandana/exampleAgents/NaturalAlliesBot.java
+++ src/ddejonge/bandana/exampleAgents/NaturalAlliesBot.java
@@ -180,10 +180,10 @@ public class NaturalAlliesBot extends ANACNegotiator {
 
     private List<BasicDeal> getDmzDealsSingleAlly(List<Power> alliesPowers) {
         List<BasicDeal> dealsToOffer = new ArrayList<>();
-        List<DMZ> demilitarizedZones = new ArrayList<>();
 
         // offer mutual DMZ deals for each ally
         for (Power power : alliesPowers) {
+            List<DMZ> demilitarizedZones = new ArrayList<>();
             if (power != this.me) {
 
                 List<Power> currentAllyList = new ArrayList<>();
@@ -205,10 +205,10 @@ public class NaturalAlliesBot extends ANACNegotiator {
 
     private List<BasicDeal> getDmzDealsMultipleAllies(List<Power> aliveAllies) {
         List<BasicDeal> dealsToOffer = new ArrayList<>();
-        List<DMZ> demilitarizedZones = new ArrayList<>();
 
         // make offers for all the coalition members to not attack each other
         for (Power ally : aliveAllies) {
+            List<DMZ> demilitarizedZones = new ArrayList<>();
             List<Power> otherCoalitionMembers = new ArrayList<>();
             otherCoalitionMembers.add(me);
 
